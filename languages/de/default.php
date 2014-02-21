58be9b91d9f3fc73484333793cb9f9991c118e76
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-02-21 12:21:48 +0100

    Fixed new version parser

--- languages/de/default.php
+++ languages/de/default.php
@@ -1,6 +1,6 @@
 <?php
 
 $GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['noContentMsg'] = 'Für diese Seite existiert leider noch kein Inhalt. Die Dokumentation wird auf <a href="%s" target="_blank">GitHub</a> gepflegt und du bist herzlich dazu eingeladen, sie zu erweitern! Vielen Dank.';
-$GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['newInVersion'] = 'Neu in Version %s:';
+$GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['newInVersion'] = 'Neu in Isotope eCommerce %s:';
 $GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['incompleteMsgTitle'] = 'Dieser Artikel ist nicht komplett!';
 $GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['incompleteMsgBody'] = 'Du kannst mithelfen, ihn zu komplettieren.';
--- library/IsotopeDocRobot/Markdown/Parsers/NewVersionParser.php
+++ library/IsotopeDocRobot/Markdown/Parsers/NewVersionParser.php
@@ -11,7 +11,15 @@ class NewVersionParser implements BeforeParserInterface
      */
     public function parseBefore($data)
     {
-        $replacement = sprintf('<docrobot_message type="info"><p><strong>%s</strong></p>$2</docrobot_message>', $GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['newInVersion']);
-        return preg_replace('#<docrobot_new_in_version version="(.*)">(.*)</docrobot_new_in_version>#U', $replacement, $data);
+        return preg_replace_callback(
+            '#<docrobot_new_in_version version="(.*)">(.*)</docrobot_new_in_version>#U',
+            function($matches) {
+
+                return sprintf('<docrobot_message type="info"><p><strong>%s</strong></p>%s</docrobot_message>',
+                    sprintf($GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['newInVersion'], $matches[1]),
+                    $matches[2]
+                );
+            },
+            $data);
     }
 }
