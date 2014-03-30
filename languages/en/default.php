c408bcb37a2ef93d5cca02adb727b74384939796
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-03-30 11:17:33 +0200

    Translation work

--- languages/en/default.php
+++ languages/en/default.php
@@ -11,3 +11,4 @@ $GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['newMsgBody'] = 'It is available since %
 $GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['feedbackFormMessageSuccess'] = 'Thank you for your feedback! We will get back at you as soon as possible!';
 $GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['feedbackFormMessageError'] = 'D\'oh! There seems to be something wrong with your form!';
 $GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['quickNavigationLabel'] = 'Quicknavigation';
+$GLOBALS['TL_LANG']['ISOTOPE_DOCROBOT']['feedbackLabel'] = 'Feedback for this article';
