415a78cd8d0b113190e5a7a031002c6e6c137e5e
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-06-23 14:55:12 +0200

    Fixed parsers

--- src/IsotopeDocRobot/Markdown/Parsers/CurrentVersionParser.php
+++ src/IsotopeDocRobot/Markdown/Parsers/CurrentVersionParser.php
@@ -2,15 +2,30 @@
 
 namespace IsotopeDocRobot\Markdown\Parsers;
 
+use IsotopeDocRobot\Context\Context;
+use IsotopeDocRobot\Markdown\ContextAwareInterface;
 use IsotopeDocRobot\Markdown\ParserInterface;
 
-class CurrentVersionParser implements ParserInterface
+class CurrentVersionParser implements ParserInterface, ContextAwareInterface
 {
+    /**
+     * @var Context
+     */
+    private $context = null;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContext(Context $context)
+    {
+        $this->context = $context;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function parseMarkdown($data)
     {
-        return str_replace('<docrobot_current_version>', $this->version, $data);
+        return str_replace('<docrobot_current_version>', $this->context->getVersion(), $data);
     }
 }
--- src/IsotopeDocRobot/Markdown/Parsers/RootParser.php
+++ src/IsotopeDocRobot/Markdown/Parsers/RootParser.php
@@ -3,15 +3,30 @@
 namespace IsotopeDocRobot\Markdown\Parsers;
 
 
+use IsotopeDocRobot\Context\Context;
+use IsotopeDocRobot\Markdown\ContextAwareInterface;
 use IsotopeDocRobot\Markdown\ParserInterface;
 
-class RootParser implements ParserInterface
+class RootParser implements ParserInterface, ContextAwareInterface
 {
+    /**
+     * @var Context
+     */
+    private $context = null;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContext(Context $context)
+    {
+        $this->context = $context;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function parseMarkdown($data)
     {
-        return str_replace('<docrobot_root>', 'system/cache/isotope/docrobot-mirror/' . $this->version, $data);
+        return str_replace('<docrobot_root>', 'system/cache/isotope/docrobot-mirror/' . $this->context->getVersion(), $data);
     }
 }
