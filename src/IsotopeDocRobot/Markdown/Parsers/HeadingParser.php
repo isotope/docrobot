984160345ccfd2639d7894441584314757efc6f2
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-07-21 17:33:44 +0200

    Support smooth scrolling

--- src/IsotopeDocRobot/Markdown/Parsers/HeadingParser.php
+++ src/IsotopeDocRobot/Markdown/Parsers/HeadingParser.php
@@ -45,7 +45,7 @@ class HeadingParser implements ParserInterface, RoutingAwareInterface
                 $content = $matches[2];
                 $id = 'deeplink-' . standardize($content);
 
-                return sprintf('<h%s id="%s">%s <a href="%s" title="%s" class="sub_permalink">#</a></h%s>',
+                return sprintf('<h%s id="%s">%s <a data-scroll href="%s" title="%s" class="sub_permalink">#</a></h%s>',
                     $level,
                     $id,
                     $content,
